7f865a518475d2d0b130fa36445414bb324026e2
Avik Mallick <avikmallick111101@gmail.com>
2021-11-23 18:43:10 +0530

    basecode (template) added

---
+++ basecode.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int               long long
+#define pb                push_back
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define vi                vector<int>
+#define vvi               vector<vector<int>>
+#define vbool             vector<bool>
+#define vvbool            vector<vector<bool>>
+#define vchar             vector<char>
+#define vvchar            vector<vector<char>>
+#define vstring           vector<string>
+#define all(x)            (x).begin, (x).end
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+
+const long long INF = 1e18;
+const int32_t M = 1e9 + 7;
+const int32_t MM = 998244353;
+
+
+void solve() {
+
+}
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+
+	return 0;
+}
