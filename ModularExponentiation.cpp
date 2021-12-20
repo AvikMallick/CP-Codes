e06659977627471cf1f8043393288730cb0d792f
Avik Mallick <73769999+AvikMallick@users.noreply.github.com>
2021-12-20 14:27:56 +0530

    Update ModularExponentiation.cpp

--- ModularExponentiation.cpp
+++ ModularExponentiation.cpp
@@ -1,22 +1,24 @@
 // Modular Exponentiation
-int mpow(int a, int b, int m) { // pow(a, b) % m
-	int x = 1;
-	while (b > 0) {
-		if (b & 1) {           // fast way of checking if b is odd
-			x = (x * a) % m;
-		}
-		a = (a * a) % m;       // compute the next power of 2
-		b >>= 1;               // this is equivalent to b = floor(b / 2)
-	}
-	return x;
+int mpow(int a, int b, int m = M) { int x = 1; while (b > 0) { if (b & 1) { x = (x * a) % m;} a = (a * a) % m; b >>= 1;} return x;
 }
 
 // Modular Inverse
-int inv(int i) {if (i == 1) return 1; return (M - ((M / i) * inv(M % i)) % M) % M;}
+int inv(int i, int m = M) {if (i == 1) return 1; return (m - ((m / i) * inv(m % i)) % m) % m;}
 
 // Modular Multiplication
-int mod_mul(int a, int b) {a = a % M; b = b % M; return (((a * b) % M) + M) % M;}
+int mod_mul(int a, int b, int m = M) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
 
 // Modular Addition
-ll mod_add(int a, int b) {a = a % M; b = b % M; return (((a + b) % M) + M) % M;}
+int mod_add(int a, int b, int m = M) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
 
+// nCr using preComputaton if factorial and inverse factorial
+// Initialize N as Needed
+int N = 1000005;
+vi fact(N + 1, 1);
+vi ifact(N + 1, 1);
+int nCr(int n, int r) {
+	int ans = fact[n];
+	ans = mod_mul(ans, ifact[r]);
+	ans = mod_mul(ans, ifact[n - r]);
+	return ans;
+}
