b74c078ff483ead4ba0dc6acca161ec0e7a18a96
Avik Mallick <73769999+AvikMallick@users.noreply.github.com>
2021-12-20 14:35:58 +0530

    Create nCr.cpp

---
+++ nCr.cpp
@@ -0,0 +1,18 @@
+// nCr using preComputaton if factorial and inverse factorial
+// Initialize N as Needed
+int N = 1000005;
+vi fact(N + 1, 1);
+vi ifact(N + 1, 1);
+int nCr(int n, int r) {
+	int ans = fact[n];
+	ans = mod_mul(ans, ifact[r]);
+	ans = mod_mul(ans, ifact[n - r]);
+	return ans;
+}
+/* initialization of ifact */
+ifact[N] = inv(fact[N]);
+for (int i = N - 1; i > 0; i--)
+{
+  ifact[i] = mod_mul(i + 1, ifact[i + 1]);
+}
+/********************************/
