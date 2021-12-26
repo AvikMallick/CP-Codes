03e7e606302633fbad640b1fb4735de183efa319
Avik Mallick <73769999+AvikMallick@users.noreply.github.com>
2021-12-26 16:33:42 +0530

    Create dsu(fast - no graph maintained).cpp

---
+++ dsu(fast - no graph maintained).cpp
@@ -0,0 +1,42 @@
+struct dsu {
+	int n;
+	vector<int> parent;
+	vector<int> count;
+	vector<int> rank;
+	void makeset(int rn) {
+		int n = rn;
+		count.assign(n, 1);
+		rank.assign(n, 0);
+		parent.assign(n, 0);
+		rep(i, 0, n) parent[i] = i;
+	}
+
+	int findParent(int x) {
+		if (parent[x] == x)
+			return x;
+		else {
+			parent[x] = findParent(parent[x]);
+			return parent[x];
+		}
+	}
+
+	void merge(int x, int y) {
+		x = findParent(x), y = findParent(y);
+		if (x == y) return;
+		if (rank[x] < rank[y])
+			swap(x, y);
+		else if (rank[x] == rank[y])
+			rank[y]++;
+		parent[y] = x;
+		count[x] += count[y];
+		return;
+	}
+
+	int size(int x) {
+		return count[findParent(x)];
+	}
+
+	bool issame(int x, int y) {
+		return findParent(x) == findParent(y);
+	}
+};
