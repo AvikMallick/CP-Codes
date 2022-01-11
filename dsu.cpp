e10a243a03b97d69e24719f9ff6c0009e096cbd9
Avik Mallick <73769999+AvikMallick@users.noreply.github.com>
2022-01-11 14:51:26 +0530

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -1,37 +1,47 @@
 struct dsu {
 	int n;
-	vector<int> parent, ranks;
-	vector<vector<int>> edges;
-
+	vector<int> parent;
+	vector<int> count;
+	vector<int> rank;
+	vector<vector<int> edges;
 	void makeset(int rn) {
-		n = rn;
-		parent = vector<int>(n);
-		ranks = vector<int>(n);
-		fill(ranks.begin(), ranks.end(), 0);
-		for (int i = 0; i < n; i++) parent[i] = i;
-		edges = vector<vector<int>>(n);
+		int n = rn;
+		edges.resize(n);
+		count.assign(n, 1);
+		rank.assign(n, 0);
+		parent.assign(n, 0);
+		rep(i, 0, n) parent[i] = i;
 	}
 
-	int findParent(int node) {
-		if (node == parent[node]) return node;
-		return (parent[node] = findParent(parent[node]));
-	}
-
-	void merge(int u, int v) {
-		int p1 = findParent(u);
-		int p2 = findParent(v);
-		if (p1 == p2) return;
-		edges[u].push_back(v);
-		edges[v].push_back(u);
-		if (ranks[p1] < ranks[p2]) {
-			parent[p1] = p2;
-		}
-		else if (ranks[p1] > ranks[p2]) {
-			parent[p2] = p1;
-		}
+	int findParent(int x) {
+		if (parent[x] == x)
+			return x;
 		else {
-			parent[p2] = p1;
-			ranks[p1]++;
+			parent[x] = findParent(parent[x]);
+			return parent[x];
 		}
+	}
+
+	void merge(int x, int y) {
+		int p1 = findParent(x);
+		int p2 = findParent(y);
+		if (p1 == p2) return;
+		edges[x].pb(y);
+		edges[y].pb(x);
+		if (rank[p1] < rank[p2])
+			swap(p1, p2);
+		else if (rank[p1] == rank[p2])
+			rank[p2]++;
+		parent[p2] = p1;
+		count[p1] += count[p2];
+		return;
+	}
+
+	int size(int x) {
+		return count[findParent(x)];
+	}
+
+	bool issame(int x, int y) {
+		return findParent(x) == findParent(y);
 	}
 };
